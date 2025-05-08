b46286052139d026bc458b31afd17ceb9c4a6b26
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-08 14:52:27 +0700

    Add ForumPage to succesfully test the message system

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,25 @@ import Error from "./components/app/Error";
 import AppLayout from "./components/app/AppLayout";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
-import Tickets from "./pages/Tickets"
-import NotificationsPage from "./pages/NotificationsPage"
-import Setting from "./pages/Setting"
+import Tickets from "./pages/Tickets";
+import NotificationsPage from "./pages/NotificationsPage";
+import Setting from "./pages/Setting";
+import ForumPage from "./testing/ForumPage";
 
 const router = createBrowserRouter([
   {
-    element: <Homepage/>,
-    path: "/home"
+    element: <Homepage />,
+    path: "/home",
   },
   {
     element: <AppLayout />,
     errorElement: <Error />,
     children: [
-      {path: "/dashboard", element: <Dashboard />},
-      {path:"/tickets", element: <Tickets/>},
-      {path: "/notifications", element: <NotificationsPage/>},
-      {path: "/settings", element: <Setting/>}
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/tickets", element: <Tickets /> },
+      { path: "/notifications", element: <NotificationsPage /> },
+      { path: "/settings", element: <Setting /> },
+      { path: "/", element: <ForumPage /> },
     ],
   },
 ]);
