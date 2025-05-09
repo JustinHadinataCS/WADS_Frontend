b4a0d1bc4a9488062d0df5c0f02464b4a182fa67
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-09 13:02:27 +0700

    Refactor the search bar UI + Buttons

--- src/components/homepage/Header.jsx
+++ src/components/homepage/Header.jsx
@@ -1,20 +1,22 @@
+import { TbLogin2 } from "react-icons/tb";
 function Header() {
   return (
     <header className="bg-[#155095] p-4 flex justify-between items-center">
       <div className="flex items-center">
         <img src="/api/placeholder/24/24" alt="Semesta Logo" className="mr-2" />
-        <span className="text-white font-semibold">Semesta Medika Support</span>
+        <span className="text-white font-bold">Semesta Medika Support</span>
       </div>
-      <div className="flex gap-4 items-center">
-        <a href="#" className="text-white hover:underline text-sm">
-          Login
-        </a>
-        <a
-          href="#"
-          className="bg-white text-blue-800 px-4 py-1 rounded-full text-sm font-medium"
-        >
+      <div className="flex gap-4 items-center font-bold ">
+        <div className="flex items-center gap-x-1 text-white hover:underline">
+          <TbLogin2 size={20} />
+          <a href="#" className="text-sm ">
+            Login
+          </a>
+        </div>
+
+        <button className="bg-white text-[#0A3E7A] px-4 py-2 rounded-full text-sm  ">
           Sign Up
-        </a>
+        </button>
       </div>
     </header>
   );
