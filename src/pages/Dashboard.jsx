244ae0e048dcfd38671f45858f0d8dd3ef6da861
Adrian <adriannugrohobasuki@gmail.com>
2025-05-10 22:01:48 +0700

    Navbar buttons and dashboard widgets now change based on the role of the logged in user

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
 import UserDashboard from "../components/app/UserDashboard";
 import AgentDashboard from "../components/app/AgentDashboard";
 import AdminDashboard from "../components/app/AdminDashboard";
+import { useAuthContext } from "../contexts/AuthContext";
 
 function Dashboard() {
+
+  const { user } = useAuthContext();
   return (
     <>
-      <AdminDashboard />
+      {user.role === "admin" && <AdminDashboard />}
+      {user.role === "agent" && <AgentDashboard />}
+      {user.role === "user" && <UserDashboard />}
     </>
   );
 }
