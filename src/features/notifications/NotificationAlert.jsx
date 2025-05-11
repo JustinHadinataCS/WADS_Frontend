4274381cee8b37afa2a73d838606ad2e92ff6189
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-11 09:32:42 +0700

    Add date-fns + uses it on the Notification Alert

--- src/features/notifications/NotificationAlert.jsx
+++ src/features/notifications/NotificationAlert.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
-function NotificationAlert({ isRead, title, children }) {
+import { formatDistanceToNow } from "date-fns";
+
+function NotificationAlert({ isRead, title, timestamp, children }) {
   const typeStyles = {
     unread: { text: "text-blue-700", bg: "bg-gray-100 border-gray-300" },
     read: { text: "text-gray-800", bg: "border-gray-300" },
@@ -11,7 +13,9 @@ function NotificationAlert({ isRead, title, children }) {
     <div className={`p-5 border-b ${bg}`}>
       <div className="flex justify-between mb-2">
         <strong className={text}>{title}</strong>
-        {/* <span className="text-gray-600 text-sm">{time}</span> */}
+        <span className="text-gray-600 text-sm">
+          {formatDistanceToNow(new Date(timestamp), { addSuffix: true })}
+        </span>
       </div>
       <p className="text-gray-800 m-0 text-sm">{children}</p>
     </div>
