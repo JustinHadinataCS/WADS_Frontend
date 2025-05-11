57a352bfd5e4e6800bf4f4531befce36b07cee74
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-11 08:47:18 +0700

    Add a Notification Context

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Error from "./components/app/Error";
 import AppLayout from "./components/app/AppLayout";
 import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
-import Tickets from "./pages/Tickets";
 import NotificationsPage from "./pages/NotificationsPage";
 import Setting from "./pages/Setting";
 import ForumPage from "./pages/ForumPage";
@@ -15,6 +14,7 @@ import SignUpPage from "./pages/SignUpPage";
 import Chatbot from "./pages/Chatbot";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./features/auth/ProtectedRoute";
+import TicketDetailsPage from "./pages/TicketDetailsPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/home" /> },
@@ -53,7 +53,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { path: "/dashboard", element: <Dashboard /> },
-      { path: "/tickets", element: <Tickets /> },
+      { path: "/tickets", element: <TicketDetailsPage /> },
       { path: "/notifications", element: <NotificationsPage /> },
       { path: "/settings", element: <Setting /> },
       { path: "/forum", element: <ForumPage /> },
---
+++ src/contexts/NotificationContext.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { useMutation } from "@tanstack/react-query";
+import { createContext, useContext, useState } from "react";
+import { login, register } from "../api/auth";
+
+const NotificationContext = createContext();
+
+function NotificationProvider({ children }) {
+  const [user, setUser] = useState("");
+
+  //   const getNotificationsMutation = useMutation({
+  //     mutationFn: getNotifications,
+  //     onSuccess: (data) => {
+  //       setUser(data);
+  //       console.log(`The data -> ${data}`);
+  //       console.log(data);
+  //       console.log(user);
+  //     },
+  //     onError: (error) => console.error(`Error: ${error.message}`),
+  //   });
+
+  //   const registerMutation = useMutation({
+  //     mutationFn: register,
+  //     onSuccess: (data) => {
+  //       setUser(data);
+  //       console.log(data);
+  //     },
+  //     onError: (error) => console.error(`Error: ${error.message}`),
+  //   });
+
+  //   const logout = () => {
+  //     setUser("");
+  //   };
+
+  return (
+    <NotificationContext.Provider>{children}</NotificationContext.Provider>
+  );
+}
+
+function useNotificationContext() {
+  const context = useContext(NotificationContext);
+  if (!NotificationContext)
+    throw new Error("Context is used outside of provider");
+  return context;
+}
+
+export { useNotificationContext, NotificationProvider };
