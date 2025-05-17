572277269e911f057ceb5cba79a0705252ff2330
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-17 16:07:34 +0700

    Refactor the Arrow Button

--- src/features/tickets/TicketPagination.jsx
+++ src/features/tickets/TicketPagination.jsx
@@ -1,21 +1,18 @@
 import { MdNavigateNext } from "react-icons/md";
 import { MdNavigateBefore } from "react-icons/md";
 import PaginationButton from "./PaginationButton";
+import ArrowButton from "./ArrowButton";
 
 export default function TicketPagination() {
   return (
     <div className="flex rounded-md shadow-sm items-center justify-between p-6 bg-white border border-gray-300">
       <div className="text-sm text-gray-700">Showing 1-3 of 24 tickets</div>
       <div className="flex gap-3">
-        <button className="px-3 py-1 border border-gray-300 bg-white hover:bg-neutral-200 hover:cursor-pointer text-sm font-medium rounded-md text-gray-700">
-          <MdNavigateBefore size={25} />
-        </button>
+        <ArrowButton type="left" />
         <PaginationButton value={1} />
         <PaginationButton value={2} />
         <PaginationButton value={3} />
-        <button className="px-3 py-1 border border-gray-300 bg-white hover:bg-neutral-200 hover:cursor-pointer text-sm font-medium rounded-md text-gray-700">
-          <MdNavigateNext size={25} />
-        </button>
+        <ArrowButton />
       </div>
     </div>
   );
