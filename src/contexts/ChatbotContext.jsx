819604d70f375867f71994b5721da49b0b9db6ed
Justin Hadinata <justinhadinata.23@gmail.com>
2025-05-29 14:40:17 +0700

    Fix the accessToken in ChatBotProvider

--- src/contexts/ChatbotContext.jsx
+++ src/contexts/ChatbotContext.jsx
@@ -5,43 +5,43 @@ import { useAuthContext } from "./AuthContext";
 
 const ChatbotContext = createContext();
 
-function ChatbotProvider({ children }){
-    const { user } = useAuthContext();
-    const queryClient = useQueryClient();
+function ChatbotProvider({ children }) {
+  const { user } = useAuthContext();
+  const queryClient = useQueryClient();
 
-    const botMessageHistoryQuery = useQuery({
-        queryKey: ["botMessageHistory"],
-        queryFn: () => getMessageHistory(user.token),
-        enabled: !!user.token,
-    })
+  const botMessageHistoryQuery = useQuery({
+    queryKey: ["botMessageHistory"],
+    queryFn: () => getMessageHistory(user.accessToken),
+    enabled: !!user.token,
+  });
 
-    const newBotMessageMutation = useMutation({
-        mutationFn: (messageData) => newMessage(user.token, messageData),
-        onSuccess: () => queryClient.invalidateQueries(['botMessageHistory']),
-        onError: (error) => console.error(`Error: ${error.message}`),
-    })
+  const newBotMessageMutation = useMutation({
+    mutationFn: (messageData) => newMessage(user.token, messageData),
+    onSuccess: () => queryClient.invalidateQueries(["botMessageHistory"]),
+    onError: (error) => console.error(`Error: ${error.message}`),
+  });
 
-    return(
-        <ChatbotContext.Provider
-            value={{
-                botMessageHistory: botMessageHistoryQuery.data,
-                botMessageHistoryLoading: botMessageHistoryQuery.isLoading,
-                botMessageHistoryError: botMessageHistoryQuery.error,
-                refetchbotMessageHistory: botMessageHistoryQuery.refetch,
+  return (
+    <ChatbotContext.Provider
+      value={{
+        botMessageHistory: botMessageHistoryQuery.data,
+        botMessageHistoryLoading: botMessageHistoryQuery.isLoading,
+        botMessageHistoryError: botMessageHistoryQuery.error,
+        refetchbotMessageHistory: botMessageHistoryQuery.refetch,
 
-                newBotMessage: newBotMessageMutation.mutate,
-                newBotMessagePending: newBotMessageMutation.isPending,
-            }}
-        >
-            {children}
-        </ChatbotContext.Provider>
-    )
+        newBotMessage: newBotMessageMutation.mutate,
+        newBotMessagePending: newBotMessageMutation.isPending,
+      }}
+    >
+      {children}
+    </ChatbotContext.Provider>
+  );
 }
 
 function useChatbotContext() {
-    const context = useContext(ChatbotContext);
-    if(!context) throw new Error("ChatbotContext is used outside of provider");
-    return context;
+  const context = useContext(ChatbotContext);
+  if (!context) throw new Error("ChatbotContext is used outside of provider");
+  return context;
 }
 
-export {useChatbotContext, ChatbotProvider}
+export { useChatbotContext, ChatbotProvider };
