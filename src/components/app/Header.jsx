ee380cda5866db6093a4f3380fc1f599597315d8
Justin Hadinata <justinhadinata.23@gmail.com>
2025-06-01 11:00:50 +0700

    Refactor Sidebar + Header for responsiveness

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,12 +1,10 @@
-import NotificationButton from "./NotificationButton";
 import PageTitle from "./PageTitle";
 import ProfileButton from "./ProfileButton";
-import SearchBar from "./SearchBar";
 import UserName from "./UserName";
 import { useLocation } from "react-router-dom";
 import { useAuthContext } from "../../contexts/AuthContext";
 
-function Header() {
+function Header({ openSidebar }) {
   const location = useLocation();
   const pathSegments = location.pathname.split("/");
   let pageName = decodeURIComponent(pathSegments[1] || "");
@@ -15,13 +13,35 @@ function Header() {
   const { user } = useAuthContext();
 
   return (
-    <div className="flex justify-between bg-white text-[#424242]  p-6  border-b-[1.5px] border-[#E5E5E5]">
-      <PageTitle title={pageName} />
-      <div className="flex justify-center items-center gap-x-4 select-none">
-        <UserName user={user} />
-        <ProfileButton user={user} />
+    <header className="bg-white shadow-sm p-4">
+      <div className="flex items-center justify-between">
+        <button
+          className="md:hidden p-2 rounded-md text-gray-600 hover:bg-gray-100"
+          onClick={openSidebar}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+
+        <PageTitle title={pageName} />
+        <div className="flex justify-center items-center gap-x-4 select-none">
+          <UserName user={user} />
+          <ProfileButton user={user} />
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
 
