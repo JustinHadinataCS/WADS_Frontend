d89c6a4e8496942d13a9e60f992bd3b517780506
Justin Hadinata <justinhadinata.23@gmail.com>
2025-06-01 18:14:09 +0700

    Delete TicketDetailsPge in feature/tickets

--- src/pages/TicketDetailsPage.jsx
+++ src/pages/TicketDetailsPage.jsx
@@ -66,27 +66,6 @@ function TicketDetailsPage() {
     <>
       <div className="flex flex-grow">
         <div className="flex-grow ">
-          <div className="bg-white p-5 rounded-md shadow-md mb-5">
-            <div className="flex justify-between items-center">
-              <h2 className="text-xl font-semibold text-gray-800">
-                #{ticketData._id} - {ticketData.title}
-              </h2>
-              <span
-                className={`px-4 py-2 font-medium rounded-md ${
-                  statusColors[ticketData.status]
-                }`}
-              >
-                {formatStatus(ticketData.status)}
-              </span>
-            </div>
-            <div className="mt-3 text-gray-600">
-              Created: {format(parseISO(ticketData.createdAt), "yyyy-MM-dd")} |
-              Last Updated:{" "}
-              {capitalizeFirstLetter(formatDistanceToNow(ticketData.updatedAt))}{" "}
-              ago
-            </div>
-          </div>
-
           <div className="grid grid-cols-7 gap-5">
             <CommunicationLog
               ticketId={ticketData._id}
