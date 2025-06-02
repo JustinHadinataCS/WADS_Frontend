ffdfeec372fd5f27be649cac20cb69109e149054
Adrian <adriannugrohobasuki@gmail.com>
2025-06-02 20:44:44 +0700

    a

--- src/features/dashboard/User/DashboardUserCard.jsx
+++ src/features/dashboard/User/DashboardUserCard.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import TicketPopup from "../../tickets/TicketPopUp";
+import TicketPopup from "../../tickets/TicketPopup";
 import ButtonBlue from "../../../components/app/ButtonBlue";
 
 function DashboardUserCard({
